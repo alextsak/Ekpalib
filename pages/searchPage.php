86095b8cef50f48dc307501343475692559197dc
takis <takis@DESKTOP-DON4EVP>
2015-12-25 20:26:43 +0200

    new table

--- pages/searchPage.php
+++ pages/searchPage.php
@@ -2,15 +2,14 @@
 	<div>
 		<h3 id="reasultsHeader">Search Results</h3>
 	</div>
-	
 	<div >
-    		<table>
-					<tr>
-						<th>Name</th>
-						<th>Address</th>
-						<th>Hours</th>
-					</tr>
-			</table>
+    	<table>
+			<tr>
+				<th>Name</th>
+				<th>Address</th>
+				<th>Hours</th>
+			</tr>
+		</table>
    	</div>
 	
 </div>
