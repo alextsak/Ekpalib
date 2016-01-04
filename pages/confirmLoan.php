e23efb11fff0c4a113687240f2d0b7ec36456535
takis <takis@DESKTOP-DON4EVP>
2016-01-04 21:06:35 +0200

    confirmloan

--- pages/confirmLoan.php
+++ pages/confirmLoan.php
@@ -10,27 +10,42 @@ if(isset($_POST['empty_cart'])){
 	}
 }
 ?>
+<style>
+#searchContainer{
+	margin-bottom:30px;
+	border: 1px solid black;
+	border-radius: 10px;
+	-moz-border-radius: 10px;
+	-webkit-border-radius: 10px;
+	height:200px;
+	border-style: double
+}
+</style>
 
+<h4 style="margin-left:400px;text-decoration: underline;margin-bottom: 25px;">Αναζήτηση βιβλίων, περιοδικών και άρθρων</h4>
 
-<div>
-	<div>
-		<h3 id="reasultsHeader">Επιβεβαίωση Δανεισμού</h3>
-		<form method="post" action="?page=searchPage"> 
-		<button type="submit" class="btn btn-primary" name="empty_cart">Καθαρισμός Καροτσιού</button> 
-		</form> 		 
-		
-	</div>
-	<div >
-    	<table>
+<div id="searchContainer" class="col-xs-12">
+		<table>
 			<?php 
         			$material = new Material();
         			$material->create_mycart($_SESSION['genre']);
        		 ?> 
 		</table>
+</div>
+
+
+	<!-- <div>
+		<h3 id="reasultsHeader">Επιβεβαίωση Δανεισμού</h3>
+		<form method="post" action="?page=searchPage"> 
+		<button type="submit" class="btn btn-primary" name="empty_cart">Καθαρισμός Καροτσιού</button> 
+		</form> 		 
+		
+	</div> -->
+	<!-- <div >
+    	
 		<div>
 			<a href="?page=confirmLoan" id="confirmLoan-Button" type="button" class="btn btn-default">Δανεισμός
       			<span class="glyphicon glyphicon-chevron-right"></span> 
     		</a>
 		</div>
-   	</div>
-</div>
+   	</div> -->
