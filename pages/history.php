679efffabe3ae42e3af6fa402ada25b6ff88337b
Panagiotis <kls.panagiotis@gmail.com>
2016-01-19 23:30:48 +0200

    history changes

--- pages/history.php
+++ pages/history.php
@@ -115,15 +115,49 @@ $user_results_history = $user->get_user_history ( $username );
 							</thead>
 							<tbody>
     		<?php
-						if ($user_results == "No data found") {
+								if ($user_results == "No data found") {
 							?>
-                	<tr>
-								<td><?php echo "Δεν υπάρχουν πρόσφατοι δανεισμοί";?></td>
+                			<tr>
+								<td><?php echo "Δεν υπάρχουν πρόσφατες αιτήσεις";?></td>
 							</tr>
-                <?php
-						} else {
-							
-						}
+                			<?php
+								} else {
+									while ( $row = $user_results->fetch(PDO::FETCH_ASSOC) ) {
+									$material = new Material ();
+									$material_info = $material->get_material_by_id($row ['MaterialID'])->fetch(PDO::FETCH_ASSOC);
+									$library = $material->get_material_library ( $row ['MaterialID'] );
+									$lib_name = '';
+									if ($library != - 1) {
+										$lib_name = $library ['Name'];
+										$lib_id = $library ['idLibraries'];
+									}
+									?>
+									<tr>
+									<td style="text-align:center;"><?php echo $material_info['title']; ?></td>
+		                   			<td style="text-align:center;"><?php echo $material_info['category']; ?></td>
+		                   			<td style="text-align:center;"><a href="javascript:detailsLibrary(<?php echo $lib_id;?>)"><?php echo $lib_name; ?></a></td>
+									<td style="text-align:center;">
+									<?php if ($row['Approved'] == 0) {?>
+											<span class="glyphicon glyphicon-ok"></span>
+											<?php 
+									}
+									else {
+										?><span class="glyphicon glyphicon-minus"></span>
+										<?php 
+									}
+									?></td>
+									<td style="width:120px;">
+									<button class="btn btn-primary btn-sm" type="button" onclick="detailsbook(<?php echo $row['MaterialID'];?>)">
+									<span class="glyphicon glyphicon-info-sign" ></span>
+									</button>
+									&nbsp | &nbsp
+									<button class="btn btn-warning btn-sm" type="submit" onclick="removeRequest(<?php echo $username;?>,<?php echo $row['MaterialID'];?>)">
+									<span class="glyphicon glyphicon-remove-circle"></span>
+									</button>
+									</td>
+		                   			</tr><?php 
+								}
+							}
 						?>
    		</tbody>
 						</table>
