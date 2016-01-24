5f47bda34aae3b068a19e6a5c123fb056c6bc557
Panagiotis <kls.panagiotis@gmail.com>
2016-01-24 14:19:54 +0200

    under construction pages

--- inc/menu.php
+++ inc/menu.php
@@ -27,25 +27,25 @@
                <li class="dropdown">
                		<a style="color:#FFFAF0;"href="#" data-toggle="dropdown" class="dropdown-toggle">Πληροφορίες <b class="caret"></b></a>
                         <ul class="dropdown-menu">
-                            <li><a style="color:#FFFAF0;" href="?page=about">Καλωσόρισμα Ββλιοθήκης</a></li>
-                            <li><a style="color:#FFFAF0;" href="#">Διοικητικό Προσωπικό</a></li>
-                            <li><a style="color:#FFFAF0;" href="#">Πολιτική Λειτουργίας</a></li>
+                            <li><a style="color:#FFFAF0;" href="?page=under_construction">Καλωσόρισμα Ββλιοθήκης</a></li>
+                            <li><a style="color:#FFFAF0;" href="?page=under_construction">Διοικητικό Προσωπικό</a></li>
+                            <li><a style="color:#FFFAF0;" href="?page=under_construction">Πολιτική Λειτουργίας</a></li>
                         </ul>
                </li>
                <li class="dropdown">
                		<a href="#" data-toggle="dropdown" class="dropdown-toggle" style="color:#FFFAF0;">Επικοινωνία <b class="caret"></b></a>
                         <ul class="dropdown-menu">
-                        	<li><a href="#" style="color:#FFFAF0;">Αποστολή Μηνύματος</a></li>
-                            <li><a href="#" style="color:#FFFAF0;">Παρατηρήσεις - Προτάσεις</a></li>
+                        	<li><a href="?page=under_construction" style="color:#FFFAF0;">Αποστολή Μηνύματος</a></li>
+                            <li><a href="?page=under_construction" style="color:#FFFAF0;">Παρατηρήσεις - Προτάσεις</a></li>
                         </ul>
                </li>
 
                <li class="dropdown">
                		<a href="?page=about" data-toggle="dropdown" class="dropdown-toggle" style="color:#FFFAF0;">Βοήθεια <b class="caret"></b></a>
                         <ul class="dropdown-menu">
-                            <li><a href="?page=about" style="color:#FFFAF0;">Συχνές Ερωτήσεις</a></li>
-                            <li><a href="#" style="color:#FFFAF0;">Οδηγίες Αναζήτησης Υλικού</a></li>
-                            <li><a href="#" style="color:#FFFAF0;">Άμεση Βοήθεια</a></li>
+                            <li><a href="?page=under_construction" style="color:#FFFAF0;">Συχνές Ερωτήσεις</a></li>
+                            <li><a href="?page=under_construction" style="color:#FFFAF0;">Οδηγίες Αναζήτησης Υλικού</a></li>
+                            <li><a href="?page=under_construction" style="color:#FFFAF0;">Άμεση Βοήθεια</a></li>
                         </ul>
                </li>
                
--- pages/under_construction.php
+++ pages/under_construction.php
@@ -1,1 +1,2 @@
-<h1>About page</h1>
+<h1>Under Construction</h1>
+<img style="width: 100%; margin: 0 auto;" src="images/under_construction.jpg">
